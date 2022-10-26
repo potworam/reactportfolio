66857659f4a05d3fb367ffb389fb621545d7ab8e
Potwora <matthewpotwora@yahoo.com>
2022-10-25 19:49:39 -0700

    fixed links and visual errors

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -16,27 +16,27 @@ function Gallery() {
   <li>
   <a href= "https://lillielovatt.github.io/eat-your-luck/" > <img src={card}  className="my-2" style={{ width: "55%" }} onClick="https://lillielovatt.github.io/eat-your-luck/" /> </a>
   </li>
-</div>,
+</div>
   <div> Employee Tracker
   <li>
   <a href= "https://github.com/potworam/employee-tracker" > <img src={employee}  className="my-2" style={{ width: "55%" }} onClick="https://github.com/potworam/employee-tracker" /> </a>
   </li>
-</div>,
+</div>
   <div> Password Generator
   <li>
   <a href= "https://potworam.github.io/password-generator2/" > <img src={password}  className="my-2" style={{ width: "55%" }} onClick="https://potworam.github.io/password-generator2/" /> </a>
   </li>
-</div>,
+</div>
   <div> Smorgasbord
   <li>
   <a href= "https://github.com/LDagnese/smorgasbord" > <img src={smorgasbord}  className="my-2" style={{ width: "55%" }} onClick="https://github.com/LDagnese/smorgasbord" /> </a>
   </li>
-</div>,
+</div>
   <div> Weather App
   <li>
   <a href= "https://potworam.github.io/weather/" > <img src={weather}  className="my-2" style={{ width: "55%" }} onClick="https://potworam.github.io/weather/" /> </a>
   </li>
-</div>,
+</div>
   <div> Work Scheduler
   <li>
   <a href= "https://potworam.github.io/calendar-app/" > <img src={workschedule}  className="my-2" style={{ width: "55%" }} onClick="https://potworam.github.io/calendar-app/" /> </a>
